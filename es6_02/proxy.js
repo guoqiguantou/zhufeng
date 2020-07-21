5416b3a2b47b87c334c7ae5f4bfaf354c13899ba
kongchao <111111>
2020-07-21 18:00:07 +0800

    proxy

--- es6_02/proxy.js
+++ es6_02/proxy.js
@@ -5,6 +5,39 @@ let obj = {
   }
 }
 
+let newObj=new Proxy(obj,{
+  set(target,key,value,receiver){
+    console.log('set',key)
+    return Reflect.set(target, key, value,receiver)
+  },
+  get(target,key,receiver){
+    console.log('get',key)
+    return Reflect.get(target, key, receiver)
+  }
+})
+newObj.name='李四'
+newObj.age.count=18
+console.log(newObj.name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // var obj1 = new Proxy({}, {
 //   get: function (target, propKey, receiver) {
 //     console.log(`getting ${propKey}!`);
